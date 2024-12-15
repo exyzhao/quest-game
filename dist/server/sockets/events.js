4ed47bded5cfa46d239fd64d3b8620f3e70b4f3c
Ethan Zhao <ethanxyzhao@gmail.com>
2024-12-15 15:44:58 -0600

    Add client

---
+++ dist/server/sockets/events.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GAME_STATE_UPDATE = exports.START_GAME = exports.JOIN_GAME = void 0;
+exports.JOIN_GAME = 'JOIN_GAME';
+exports.START_GAME = 'START_GAME';
+exports.GAME_STATE_UPDATE = 'GAME_STATE_UPDATE';
